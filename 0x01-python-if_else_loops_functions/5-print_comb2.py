24f6e143c9e29ce6da6f33039c111ee61b701112
Melek78 <abemelek78@gmail.com>
2024-03-12 17:28:51 +0300

    from 0 to 6

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for number in range(0, 100):
+    if number == 99:
+        print(number)
+    else:
+        print(F'{number:02}', end= ', ')
---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for a in range(0,10):
+    for b in range(0,10):
+        if a < b:
+            print(a,b, end= ', ')
+        
