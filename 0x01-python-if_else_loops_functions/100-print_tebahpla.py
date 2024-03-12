5c8792d37fe98d1a165057711a58191112feb3eb
Melek78 <abemelek78@gmail.com>
2024-03-12 18:25:25 +0300

    advanced

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-i = 122
-while i > 96:
-    print("{}".format(chr(i) if i % 2 == 0 else chr(i - 32)), end="")
-    i -= 1
+for i in reversed(range(97, 123)):
+    if i % 2 != 0:
+        i = i - 32
+    print('{}'.format(chr(i)), end='')
