6064c10efcad48688ddd61ff9427708b50d89239
Melek78 <abemelek78@gmail.com>
2024-03-12 17:42:09 +0300

    error correction

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for a in range(0,10):
+for a in range(0,9):
     for b in range(0,10):
-        if a < b:
-            print(a,b, end= ', ')
-        
+        if a == 8 and b == 9:
+            print('{}{}'.format(a,b))
+        else:
+            print('{}{}'.format(a,b), end= ', ')
