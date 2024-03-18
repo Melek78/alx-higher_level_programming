c6b0d10481ff998ff5194f6d8c58ffdceafbc843
Melek78 <abemelek78@gmail.com>
2024-03-18 21:14:10 +0300

    correction 100

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,29 +1,30 @@
-
-#include <stdio.h>
-#include <Python.h>
+#include "Python.h"
+#include "stdio.h"
 
 /**
- * print_python_list_info - prints info about a python list
- * @p: PyObject
- * 
- *Return: void 
+ * print_python_list_info - prints some basic info about Python lists
+ * @p: the list object from Python whose elements and length will be printed
+ *
+ * Return: void
  */
-
 void print_python_list_info(PyObject *p)
 {
-    long int size, i;
-    PyListObject *list;
-    PyObject *item;
+	int i;
 
-    size = Py_SIZE(p);
-    printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Size of the Python List = %d\n", (int) PyList_Size(p));
+	printf("[*] Allocated = %d\n", (int) PyByteArray_Size(p));
 
-    list = (PyListObject *)p;
-    printf("[*] Allocated = %ld\n", list->allocated);
-
-    for (i = 0; i < size; i++)
-    {
-        item = PyList_GetItem(p, i);
-        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
-    }
+	for(i = 0; i < (int) PyList_Size(p); i++)
+	{
+		if (PyLong_Check(PyList_GetItem(p, i)))
+			printf("Element %d: int\n", i);
+		else if (PyFloat_Check(PyList_GetItem(p, i)))
+			printf("Element %d: float\n", i);
+		else if (PyUnicode_Check(PyList_GetItem(p, i)))
+			printf("Element %d: str\n", i);
+		else if (PyList_Check(PyList_GetItem(p, i)))
+			printf("Element %d: list\n", i);
+		else if (PyTuple_Check(PyList_GetItem(p, i)))
+			printf("Element %d: tuple\n", i);
+	}
 }
